180a455460af0fcbed03eecf194ea59ca962fee1
Islu <luigui.angel07@hotmail.com>
2025-05-18 13:34:49 -0600

    Add application icons and manifest for BoxShadow Studio

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,6 +40,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta name="apple-mobile-web-app-title" content="BoxShadow Studio" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,13 @@
+import { Icons } from "./Icons"
+
 export default function Header() {
   return (
     <div className="bg-background fixed top-0 z-50 flex h-16 w-full items-center border-b px-3">
       <div className="flex items-end gap-2">
-        <h1 className="text-xl leading-none font-bold">BoxShadow Studio</h1>
-        <span className="text-muted-foreground text-xs leading-none">
-          by Luis Cobian
-        </span>
+        <h1 className="inline-flex items-center gap-1 text-xl leading-none font-bold">
+          <Icons.BoxStudio className="size-10" />
+          BoxShadow Studio
+        </h1>
       </div>
     </div>
   )

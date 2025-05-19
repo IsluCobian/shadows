c0f99d3421dcba67b8ff773a9e9bfcf3211ef16e
Islu <luigui.angel07@hotmail.com>
2025-05-19 12:48:10 -0600

    feat: restructure Header component layout for improved styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,10 +4,13 @@ export default function Header() {
   return (
     <div className="bg-background fixed top-0 z-50 flex h-16 w-full items-center border-b px-3">
       <div className="flex items-end gap-2">
-        <h1 className="inline-flex items-center gap-1 text-xl leading-none font-bold">
-          <Icons.BoxStudio className="size-10" />
-          BoxShadow Studio
-        </h1>
+        <div className="flex items-center gap-1">
+          <Icons.BoxStudio className="size-12" />
+          <h1 className="flex flex-col text-xl leading-none font-bold">
+            BoxShadow
+            <span className="-mt-1.5 text-base font-normal"> Studio</span>
+          </h1>
+        </div>
       </div>
     </div>
   )
